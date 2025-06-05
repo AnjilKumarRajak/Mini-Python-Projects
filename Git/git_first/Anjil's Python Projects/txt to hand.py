0277c614c0fda243e5fd87dfcfb21683a2acbdbc
Anjil_Kumar <anjilkumar098@gmail.com>
2025-06-05 17:48:59 +0530

    Sixth Project

---
+++ Git/git_first/Anjil's Python Projects/txt to hand.py
@@ -0,0 +1,4 @@
+import pywhatkit as pw 
+txt="Hey, I was looking for an individual who can truly handle our company and contribute a lot in its various sectors."
+pw.text_to_handwriting(txt)
+print("end")
